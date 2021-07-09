2ea7449954ff5f8d40c8c7fe786a1b985fe3e53e
Austin <austinshore@icloud.com>
2021-07-09 13:33:14 -0600

    initial commit

---
+++ jsexample.js
@@ -0,0 +1,8 @@
+function changeClass(){
+  
+    document.getElementById("changeClass").className = "blue";  
+}   
+  function changeText(){
+
+    document.getElementById("changeText").innerHTML = "you changed this text";
+}
